6b734acae967fa635e3c855cf3d31f249ba4a056
Imam Ghazi Khan <77143811+Imam-Ghazi-Khan@users.noreply.github.com>
2023-01-15 17:59:19 +0530

    fixed contact form

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sendEmail(){
     Email.send({
         SecureToken : "7838fb63-f763-4a3a-b379-5224137dc6dc",
         To : "igkhan1710@gmail.com",
-        From : document.getElementById("email").value,
+        From : "igkhan1710@gmail.com",
         Subject : "from "+document.getElementById("email").value,
         Body : document.getElementById("message").value,
     }).then(
